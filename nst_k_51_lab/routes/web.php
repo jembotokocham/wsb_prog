c283a213416c7aefd4c89236048e06c147fdd609
Maria Gierszon <gierszonmaria@gmail.com>
2023-09-24 09:03:59 +0200

    cwiczenie 1

--- nst_k_51_lab/routes/web.php
+++ nst_k_51_lab/routes/web.php
@@ -29,3 +29,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('wsb', function(){
+    return view('wsb',['firstName'=>'Janusz','lastName'=>'Nowak']);
+});
+
